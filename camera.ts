fdce07b4c3a23aa581f6da67458a1e4aa3fa7e7d
giselle88 <giselle.chan@smarthon.cc>
2020-01-24 12:29:40 +0800

    upload camera.ts

---
+++ camera.ts
@@ -0,0 +1,64 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#6495ED icon="\uf030"
+namespace Camera {
+
+    // -------------- 1. Intialize Camera ----------------
+    //% blockId=initialize_camera
+    //% block="Initialize Smarthon Camera Module"   
+    //% weight=100
+    //% blockGap=7
+    export function initizlizeCamera(): void {
+        serial.redirect(
+            SerialPin.P16,
+            SerialPin.P8,
+            BaudRate.BaudRate115200
+        )
+    }
+    // -------------- 2. Start Face Detection ----------------
+    //% blockId=Camera_FaceDetection
+    //% block="Start Face Detection"
+    //% weight=90
+    //% blockGap=7
+    export function FaceDetection(): void {
+        serial.writeString("faces")
+    }
+    // -------------- 3. Start Object Detection ----------------
+    //% blockId=Camera_ObjDetection
+    //% block="Start Object Detection"
+    //% weight=80
+
+    export function ObjDetection(): void {
+        serial.writeString("object")
+    }
+    // -------------- 4. Return Detection Value ----------------
+    //% blockId=Camera_dataResponse_faces
+    //% block="Get Human Faces (detected or not)"
+    //% weight=70
+    //% blockGap=7
+    export function DataResponse_faces(): string {
+        let temp = ""
+        temp = serial.readLine()
+        serial.writeString("got")
+        return temp;
+    }
+
+    //% blockId=Camera_dataResponse_obj
+    //% block="Get Camera Object Name (string)"
+    //% weight=60
+    export function DataResponse_obj(): string {
+        let temp = ""
+        temp = serial.readLine()
+        serial.writeString("got")
+        return temp;
+    }
+
+
+}

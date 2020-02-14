0e055f467c7cc9f3f29912ca4d87937e06cb5c8f
Giselle <giselle.chan@smarthon.cc>
2020-02-14 15:54:46 +0800

    for upload testing

--- camera.ts
+++ camera.ts
@@ -1,6 +1,6 @@
 
 /**
- * Use this file to define custom functions and blocks.
+ * Use this file to define custom functions and blocks. 
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
